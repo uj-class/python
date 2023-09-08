773ef64b57132f31e6d9e0f24c345e70b4b39705
Ujjwal Singh <ujjwalsingh@outlook.com>
2023-09-08 17:23:52 +0530

    Division by substraction

---
+++ dbs.py
@@ -0,0 +1,13 @@
+#DBS - Divide by Substract
+
+a = int( input("enter 1st number:") )
+b = int( input("enter 2nd number:") )
+
+x = a
+i = 0
+
+while(x != 0):
+    x = x-b
+    i += 1
+    
+print(i)
